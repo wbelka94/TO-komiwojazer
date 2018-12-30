b2f8b7d44936efa6a68ac763a61b3f838950eb84
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-12-30 17:22:43 +0100

    Find next point in list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.GreedyCycle;
 import models.Point;
 import utils.TSPFileParser;
 import utils.EuclideanDist;
@@ -8,10 +9,7 @@ public class Main {
     public static void main(String [ ] args) {
         List<Point> points = TSPFileParser.readData("data/kroA100.tsp");
         System.out.println(points.get(99).x);
-        for (Point point:points) {
-
-            System.out.println(point.x);
-        }
+        System.out.println(GreedyCycle.nextPoint(points, points.get(50)).x);
 
     }
 }
--- src/algorithms/GreedyCycle.java
+++ src/algorithms/GreedyCycle.java
@@ -1,5 +1,28 @@
 package algorithms;
 
-public class GreedyCycle {
+import models.Point;
+import utils.EuclideanDist;
 
+import java.util.*;
+
+public class GreedyCycle {
+    public static Map<Point, Double> distanceLengths = new HashMap<>();
+
+    public static Point nextPoint(List<Point> points, Point startingPoint) {
+        points.remove(50);
+        for (Point point:points) {
+            distanceLengths.put(point, EuclideanDist.calc(startingPoint, point));
+        }
+
+//        System.out.println(points.get(50).x);
+
+        Map.Entry<Point, Double> min = null;
+        for (Map.Entry<Point, Double> entry : distanceLengths.entrySet()) {
+            if (min == null || min.getValue() > entry.getValue()) {
+                min = entry;
+            }
+        }
+        System.out.println(min.getValue());
+        return min.getKey();
+    }
 }
