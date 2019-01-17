78339d57cf7f82640cf028cb2c4ee82e97c1a354
wbelka94 <wbelka94@gmail.com>
2019-01-17 20:58:39 +0100

    LP algorithm

---
+++ src/algorithms/LP.java
@@ -0,0 +1,59 @@
+package algorithms;
+
+import models.Point;
+import utils.EuclideanDist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LP extends Algorithm {
+    private List<Point> currentPath;
+    private double currentTotalDistance;
+
+    public LP(List<Point> path) {
+        super(path);
+        this.currentPath = path;
+        this.currentTotalDistance = EuclideanDist.calcForPath(currentPath);
+        System.out.println(this.currentTotalDistance);
+    }
+
+    @Override
+    public List<Point> arrangePoints() {
+        boolean next;
+        do {
+            next = false;
+            List<List<Point>> newPaths = this.twoOpt();
+            for (List<Point> path : newPaths) {
+                double newDistance = EuclideanDist.calcForPath(path);
+                if (newDistance < this.currentTotalDistance) {
+                    this.currentTotalDistance = newDistance;
+                    System.out.println(this.currentTotalDistance);
+                    this.currentPath = path;
+                    next = true;
+                }
+            }
+        } while (next);
+
+        return this.currentPath;
+    }
+
+    private List<List<Point>> twoOpt() {
+        List<List<Point>> N = new ArrayList<>();
+        int pathLength = currentPath.size();
+        for (int i = 0; i < pathLength - 1; i++) {
+            for (int j = i + 1; j < pathLength - 1; j++) {
+                List<Point> l1 = new ArrayList<>(currentPath.subList(0, i));
+                List<Point> l2 = new ArrayList<>(currentPath.subList(i, j));
+                Collections.reverse(l2);
+                List<Point> l3 = new ArrayList<>(currentPath.subList(j, pathLength));
+                List<Point> newPath = new ArrayList<>();
+                newPath.addAll(l1);
+                newPath.addAll(l2);
+                newPath.addAll(l3);
+                N.add(newPath);
+            }
+        }
+        return N;
+    }
+}
--- src/utils/EuclideanDist.java
+++ src/utils/EuclideanDist.java
@@ -2,8 +2,19 @@ package utils;
 
 import models.Point;
 
+import java.util.List;
+
 public class EuclideanDist {
     public static double calc(Point a, Point b) {
         return Math.sqrt(Math.pow((a.x - b.x), 2) + Math.pow((a.y - b.y), 2));
+    }
+
+    public static double calcForPath(List<Point> path) {
+        double distance = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            distance += calc(path.get(i), path.get(i + 1));
+        }
+        distance += calc(path.get(path.size() - 1), path.get(0));
+        return distance;
     }
 }
